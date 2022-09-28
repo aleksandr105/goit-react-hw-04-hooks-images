0d38d10e27f7d0566947369ee3aa5bd5a42542fe
aleksandr105 <aleksandrsherbina105@gmail.com>
2022-09-28 16:09:45 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,13 @@ import { LoadMore } from 'components/ButtonLoadMore/LoadMore';
 import { Spiner } from 'components/Loader/Loader';
 import { useState, useEffect } from 'react';
 
-let totalElSearch = null;
-
 export const App = () => {
   const [dataGallery, setDataGallery] = useState([]);
   const [status, setStatus] = useState('idle');
   const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
   const [page, setPage] = useState(1);
+  const [totalElSearch, setTotalElSearch] = useState(null);
 
   useEffect(() => {
     if (searchQuery === '') {
@@ -23,12 +22,11 @@ export const App = () => {
     searchImages(searchQuery, page)
       .then(dataSearch => {
         if (dataSearch.hits.length === 0) {
-          totalElSearch = null;
           return Promise.reject(
             `Can't find ${searchQuery} :-(, try something else`
           );
         } else {
-          totalElSearch = dataSearch.totalHits;
+          setTotalElSearch(dataSearch.totalHits);
           setDataGallery(prev => [...prev, ...dataSearch.hits]);
           setError(null);
         }
@@ -44,11 +42,11 @@ export const App = () => {
     }
 
     if (searchData === searchQuery) {
-      resetForm();
+      alert('request is the same as before');
       return;
     }
 
-    totalElSearch = null;
+    setTotalElSearch(null);
     setPage(1);
     setSearchQuery(searchData);
     setStatus('pending');
